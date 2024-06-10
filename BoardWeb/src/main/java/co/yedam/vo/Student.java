b597b0818500d363aa5b2899287fd7a6f7705dfe
admin <admin@YD01-14>
2024-06-10 17:49:41 +0900

    BoardWeb 추가

---
+++ BoardWeb/src/main/java/co/yedam/vo/Student.java
@@ -0,0 +1,16 @@
+package co.yedam.vo;
+
+import java.sql.Date;
+
+import lombok.Data;
+
+
+@Data
+public class Student {
+	private String stdNo;
+	private String stdName;
+	private String phone;
+	private String bldType;
+	private Date createDate;
+	
+}
